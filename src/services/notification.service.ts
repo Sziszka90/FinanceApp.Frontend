332810539d48ce4234a89d4dcca7c84ae06cec18
Szilard Ferencz <szilard.fer@gmail.com>
2025-07-25 11:01:38 +0200

    sdg

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -30,7 +30,7 @@ export class NotificationService {
 
   startConnection(): void {
     this.hubConnection = new HubConnectionBuilder()
-      .withUrl(`/notificationHub`, {
+      .withUrl(`https://www.financeapp.fun/notificationHub`, {
         transport: HttpTransportType.LongPolling,
       })
       .withAutomaticReconnect()
