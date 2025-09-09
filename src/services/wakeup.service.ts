cfa6147281ddea43b930a05818acd35e177b887c
Szilard Ferencz <szilard.fer@gmail.com>
2025-09-09 20:20:32 +0200

    Fix

--- src/services/wakeup.service.ts
+++ src/services/wakeup.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, firstValueFrom, of, throwError } from 'rxjs';
 import { timeout, catchError, retry } from 'rxjs/operators';
 import { environment } from '../environments/environment';
